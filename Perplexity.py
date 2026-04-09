e4ed7ebaa8aca996b07d86c027b4a1841400722d
chris gogolos <t8240033@aueb.gr>
2026-04-09 19:10:45 +0300

    argparse #1

---
+++ Perplexity.py
@@ -0,0 +1,26 @@
+import torch
+import math
+import argparse
+from transformers import AutoTokenizer, AutoModelForCausalLM
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--stride",help="Stride size for moving window", type=int, default=512)
+parser.add_argument("--n-ctx",help="the window size", type=int, default=2048)
+parser.add_argument("--begin-context-tokens",help="the number of pieces to be used as content in the first window",
+                     type=int, default=512)
+parser.add_argument('input_file', type=str, help='the file where the computing perplexity is evaluated')
+parser.add_argument("--output_file", type=str, help="the file in which the program results will be saved")
+
+model_name = "facebook/opt-125m"
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+model = AutoModelForCausalLM.from_pretrained(
+    model_name, tie_word_embeddings=False
+)
+model.eval()
+
+filename = input("Enter file name: ")
+with open(filename, "r") as f:
+    text = f.read()
+
+tokens = tokenizer(text).input_ids
+
